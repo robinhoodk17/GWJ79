6e5b2654740e9773e9fcb162d0a535f8615f2121
robinhoodk17 <fifthsunrevolution1@gmail.com>
2025-03-16 09:26:42 +0100

    camera improvements

--- godot-4-jam-template-main/source/player/camera_controls.gd
+++ godot-4-jam-template-main/source/player/camera_controls.gd
@@ -4,7 +4,10 @@ enum camera_state{LOCK_ON, NORMAL, CUTSCENE}
 
 @export var rotate_action: GUIDEAction
 @export var camera : Camera3D
+@export var follow_target : Marker3D
+@export var original_target : Node3D
 @export var sensibility : float = 1.0
+@export var player : CharacterBody3D
 
 var offset : Vector3
 var current_camera_state : camera_state = camera_state.NORMAL
@@ -18,6 +21,10 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	match current_camera_state:
 		camera_state.NORMAL:
+			var position_lerp_strength = delta * sensibility * 2.0
+			if !player.velocity:
+				position_lerp_strength *= 2.0
+			follow_target.position = lerp(follow_target.position, original_target.position + player.velocity.normalized()*2.0, position_lerp_strength)
 			var rotation_input : Vector2 = rotate_action.value_axis_2d
 			rotation.x -= rotation_input.y * sensibility * delta
 			rotation.x = clamp(rotation.x, -PI/2, PI/4)
--- godot-4-jam-template-main/source/player/camera_follow.gd
+++ godot-4-jam-template-main/source/player/camera_follow.gd
@@ -3,6 +3,8 @@ extends Camera3D
 @export var player : CharacterBody3D
 @export var spring_position : Node3D
 @export var lerp_power : float = 10.0
+@export var follow_target : Marker3D
 
 func _process(delta: float) -> void:
 	global_position = lerp(global_position, spring_position.global_position, delta*lerp_power)
+	look_at(follow_target.global_position)
