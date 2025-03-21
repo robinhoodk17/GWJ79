24a2b76b7a8d11b9b1411bf580da3414598dd7a8
robinhoodk17 <fifthsunrevolution1@gmail.com>
2025-03-21 20:27:53 +0100

    made lock_on togglable

--- godot-4-jam-template-main/scripts/terrain/terrain_events/queue_free_on_combat_finish.gd
+++ godot-4-jam-template-main/scripts/terrain/terrain_events/queue_free_on_combat_finish.gd
@@ -1,6 +1,6 @@
 extends Node3D
 
-@export var which_combat : Finish_Combat
+@export var which_combat : Combat
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Signalbus.combat_finished.connect(on_combat_finished)
@@ -9,5 +9,4 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func on_combat_finished(_body : Node3D) -> void:
 	if _body == which_combat:
-		print_debug("detected finished combat")
 		queue_free()
